605873ead0bb84692ef64fa73df0bc80210cd597
codingtokki <88866019+codingtokki@users.noreply.github.com>
2022-05-17 12:19:44 -0700

    first attempts in integrating Square API

--- server.py
+++ server.py
@@ -26,6 +26,12 @@ from model import BusinessUser, connect_to_db
 from sqlalchemy.sql import text
 import crud
 from jinja2 import StrictUndefined
+# from square.client import Client
+# from dotenv import load_dotenv
+import os
+import urllib.request, json
+
+
 
 
 app = Flask(__name__)
@@ -33,10 +39,11 @@ bcrypt = Bcrypt(app)
 app.secret_key = '*xZJ_0d7c#+ii0C'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
-# @app.route('/')
-# pass 
+APPLICATION_ID = os.environ['APPLICATION_ID']
 
-# @app.route('/customers')
-# def get_customers():
-#     url = 
+@app.route('/')
+def sample():
+    return "<p>Hello</p>"
 
+if __name__ == '__main__':
+    app.run(debug=True)
